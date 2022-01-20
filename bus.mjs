8e69ad133c70fea450a11e91c3b1d6db8098d3e8
Sam James <Sam.James@softwire.com>
2022-01-20 14:22:32 +0000

    sorted busses correctly

--- bus.mjs
+++ bus.mjs
@@ -11,11 +11,18 @@ function getData(){
     
     fetch(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals?api-key=${API_KEY}`)
         .then(response => response.json())
-        .then(body => {
-            for (let i=0; i<5; i++){
-            console.log(body[i]);
-                }
-            });
-}
+        .then(busses => {
+            busses.sort((bus1, bus2) => bus1.timeToStation - bus2.timeToStation );
+            for (const bus of busses){
+                console.log(
+                    "Bus to " +
+                    bus.destinationName +
+                    " arriving in around " +
+                    Math.floor(bus.timeToStation/60) +
+                    " minutes."
+                );
+            }
+        });
+    }
 
 getData();
