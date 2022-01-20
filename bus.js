472ec2210279f9b5b75d97de8789178d9edba41a
Sam James <Sam.James@softwire.com>
2022-01-20 11:54:58 +0000

    refactored into function

--- bus.js
+++ bus.js
@@ -3,9 +3,13 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const ps = require('prompt-sync');
 const prompt = ps();
 
-API_KEY = "75d6773ad7b647d199cd728959050908";
-const id = prompt("Please specify a Location ID:");
+function getData(){
+    API_KEY = "75d6773ad7b647d199cd728959050908";
+    const id = prompt("Please specify a Location ID:");
+    
+    fetch(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals?api-key=${API_KEY}`)
+        .then(response => response.json())
+        .then(body => console.log(body));
+}
 
-fetch(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals?api-key=${API_KEY}`)
-    .then(response => response.json())
-    .then(body => console.log(body));
+getData();
