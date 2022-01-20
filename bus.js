e4cd494789b8a326c53fa78b14746e67542af2a1
Sam James <Sam.James@softwire.com>
2022-01-20 11:33:17 +0000

    added request functionality

---
+++ bus.js
@@ -0,0 +1,11 @@
+// const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const ps = require('prompt-sync');
+const prompt = ps();
+
+API_KEY = "75d6773ad7b647d199cd728959050908";
+const id = prompt("Please specify a Location ID:");
+
+fetch(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals?api-key=${API_KEY}`)
+    .then(response => response.json())
+    .then(body => console.log(body));
