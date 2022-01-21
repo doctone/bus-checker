cd7182ed9148c92a7b4d1d5c1918260360b6deec
Sam James <Sam.James@softwire.com>
2022-01-21 11:26:50 +0000

    refactored with better ordered functions

--- bus.mjs
+++ bus.mjs
@@ -3,26 +3,6 @@ import fetch from "node-fetch";
 const prompt = ps();
 const API_KEY = "75d6773ad7b647d199cd728959050908";
 
-function nextArrivalTime(id){
-    
-    fetch(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals?api-key=${API_KEY}`)
-        .then(response => response.json())
-        .then(busses => {
-            busses.sort((bus1, bus2) => bus1.timeToStation - bus2.timeToStation );
-            for (const bus of busses){
-                console.log(
-                    "Bus to " +
-                    bus.destinationName +
-                    " arriving in around " +
-                    Math.floor(bus.timeToStation/60) +
-                    " minutes."
-                );
-            }
-        });
-    }
-
-// retrieve long/lats from Postcode API
-
 async function getStopsByPostcode(){
     const postCode = prompt("Please specify a postcode: ");
     
@@ -32,7 +12,7 @@ async function getStopsByPostcode(){
     const latitude = body.result.latitude;
     
     // get closest stops from TFL API using long/lat
-    const validStop = [
+    const validStops = [
         "CarPickupSetDownArea",
         "NaptanAirAccessArea",
         "NaptanAirEntrance",
@@ -71,11 +51,9 @@ async function getStopsByPostcode(){
         "NaptanTaxiRank",
         "NaptanUnmarkedPoint",
         "TransportInterchange"
-      ]
+      ].join(',');
     
-    const joinedTypes = validStop.join(',');
-    
-    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=${joinedTypes}&radius=1500`);
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=${validStops}&radius=1500`);
     const busStops = await response.json()
     const stops = busStops.stopPoints;
     stops.sort((stop1, stop2) => stop1.distance - stop2.distance);
@@ -92,4 +70,22 @@ async function getStopsByPostcode(){
     console.log("-----------------------");
     nextArrivalTime(busId);
 }
+
+function nextArrivalTime(id){
+    
+    fetch(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals?api-key=${API_KEY}`)
+        .then(response => response.json())
+        .then(busses => {
+            busses.sort((bus1, bus2) => bus1.timeToStation - bus2.timeToStation );
+            for (let i = 0; i<5; i++){
+                console.log(
+                    "Bus to " +
+                    busses[i].destinationName +
+                    " arriving in around " +
+                    Math.floor(busses[i].timeToStation/60) +
+                    " minutes."
+                );
+            }
+        });
+    }
 getStopsByPostcode();
