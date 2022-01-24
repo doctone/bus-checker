fcfc4ae6e496d95c0586bbec84aaa06b38982351
Sam James <Sam.James@softwire.com>
2022-01-24 16:24:14 +0000

    added loop to error handling

--- bus.mjs
+++ bus.mjs
@@ -1,83 +1,116 @@
 import ps from 'prompt-sync';
 import fetch from "node-fetch";
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
 const prompt = ps();
 const API_KEY = "75d6773ad7b647d199cd728959050908";
 
 async function getStopsByPostcode(){
+    let postcodeResponse;
+    let body = ''
     const postCode = prompt("Please specify a postcode: ");
-    
-    const postcodeResponse = await fetch(`http://api.postcodes.io/postcodes/${postCode}`);
-    try {
-        const body = await postcodeResponse.json();
-    
-        const longitude = body.result.longitude;
-        const latitude = body.result.latitude;
-        
-        // get closest stops from TFL API using long/lat
-        const validStopTypes = [
-            "CarPickupSetDownArea",
-            "NaptanAirAccessArea",
-            "NaptanAirEntrance",
-            "NaptanAirportBuilding",
-            "NaptanBusCoachStation",
-            "NaptanBusWayPoint",
-            "NaptanCoachAccessArea",
-            "NaptanCoachBay",
-            "NaptanCoachEntrance",
-            "NaptanCoachServiceCoverage",
-            "NaptanCoachVariableBay",
-            "NaptanFerryAccessArea",
-            "NaptanFerryBerth",
-            "NaptanFerryEntrance",
-            "NaptanFerryPort",
-            "NaptanFlexibleZone",
-            "NaptanHailAndRideSection",
-            "NaptanLiftCableCarAccessArea",
-            "NaptanLiftCableCarEntrance",
-            "NaptanLiftCableCarStop",
-            "NaptanLiftCableCarStopArea",
-            "NaptanMarkedPoint",
-            "NaptanMetroAccessArea",
-            "NaptanMetroEntrance",
-            "NaptanMetroPlatform",
-            "NaptanMetroStation",
-            "NaptanOnstreetBusCoachStopCluster",
-            "NaptanOnstreetBusCoachStopPair",
-            "NaptanPrivateBusCoachTram",
-            "NaptanPublicBusCoachTram",
-            "NaptanRailAccessArea",
-            "NaptanRailEntrance",
-            "NaptanRailPlatform",
-            "NaptanRailStation",
-            "NaptanSharedTaxi",
-            "NaptanTaxiRank",
-            "NaptanUnmarkedPoint",
-            "TransportInterchange"
-          ].join(',');
-        
-        const response = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=${validStopTypes}&radius=500`);
-        const busStops = await response.json()
-        const stops = busStops.stopPoints;
-        let validStops = []
-        if (stops.length === 0) {
-            console.log("Did not return any results.");
-        }
-        else {
-            stops.sort((stop1, stop2) => stop1.distance - stop2.distance);
-            for (const stop of stops){
-                if (stop.children.length !== 0){
-                    validStops.push(stop);
-                    }
-                }
-            await printNearestStops(validStops, postCode);
-            // console.log(stop1,stop2);
+    do {
+        try {
+            postcodeResponse = await fetch(`http://api.postcodes.io/postcodes/${postCode}`);
+            body = await postcodeResponse.json();
+            if (!body.result){
+                throw 'not a valid postcode';
             }
+        } catch (err) {
+            console.log("Sorry! Invalid postcode");
+            logger.error('invalid postcode');
         }
-    catch (err) {
-        console.log("Sorry! Invalid postcode");
-        getStopsByPostcode();
     }
+    while (body === false);
+    
+    const longitude = body.result.longitude;
+    const latitude = body.result.latitude;
+  
+    // get closest stops from TFL API using long/lat
+    const validStopTypes = [
+        "CarPickupSetDownArea",
+        "NaptanAirAccessArea",
+        "NaptanAirEntrance",
+        "NaptanAirportBuilding",
+        "NaptanBusCoachStation",
+        "NaptanBusWayPoint",
+        "NaptanCoachAccessArea",
+        "NaptanCoachBay",
+        "NaptanCoachEntrance",
+        "NaptanCoachServiceCoverage",
+        "NaptanCoachVariableBay",
+        "NaptanFerryAccessArea",
+        "NaptanFerryBerth",
+        "NaptanFerryEntrance",
+        "NaptanFerryPort",
+        "NaptanFlexibleZone",
+        "NaptanHailAndRideSection",
+        "NaptanLiftCableCarAccessArea",
+        "NaptanLiftCableCarEntrance",
+        "NaptanLiftCableCarStop",
+        "NaptanLiftCableCarStopArea",
+        "NaptanMarkedPoint",
+        "NaptanMetroAccessArea",
+        "NaptanMetroEntrance",
+        "NaptanMetroPlatform",
+        "NaptanMetroStation",
+        "NaptanOnstreetBusCoachStopCluster",
+        "NaptanOnstreetBusCoachStopPair",
+        "NaptanPrivateBusCoachTram",
+        "NaptanPublicBusCoachTram",
+        "NaptanRailAccessArea",
+        "NaptanRailEntrance",
+        "NaptanRailPlatform",
+        "NaptanRailStation",
+        "NaptanSharedTaxi",
+        "NaptanTaxiRank",
+        "NaptanUnmarkedPoint",
+        "TransportInterchange"
+        ].join(',');
+    
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=${validStopTypes}&radius=500`);
+    const busStops = await response.json()
+    const stops = busStops.stopPoints;
+    let validStops = []
+    if (stops.length === 0) {
+        console.log("Did not return any results.");
+    }
+    else {
+        stops.sort((stop1, stop2) => stop1.distance - stop2.distance);
+        for (const stop of stops){
+            if (stop.children.length !== 0){
+                validStops.push(stop);
+                }
+            }
+        await printNearestStops(validStops, postCode);
+        // console.log(stop1,stop2);
+        }
 }
+
 async function printNearestStops(validStops, postCode){
     console.log('-----------------------');
     console.log('---BUS ARRIVAL TIMES---');
@@ -120,18 +153,17 @@ async function fetchJourney (postCode, naptanId){
 
     let response = await fetch(url);
     let data = await response.json();
-    //console.log(data.journeys);
     let numberOfWays = data.journeys.length;
     let wayNr=1;
     for (let j in data.journeys) {
-        //console.log (data.journeys[j].legs);
+
         if (numberOfWays>0) {
             console.log ("ROUTE NR "+wayNr.toString());
             wayNr++;
         }
         for (const leg of data.journeys[j].legs) {
             console.log ("SUMMARY: "+leg.instruction.summary);
-            //console.log(leg.instruction.steps);
+
             if (leg.instruction.steps.length>0) {
                 console.log ("STEPS: ");
                 for (let st in leg.instruction.steps) {
@@ -143,4 +175,8 @@ async function fetchJourney (postCode, naptanId){
     }
 }
 
-getStopsByPostcode();
+function Main(){
+    getStopsByPostcode();
+}
+
+Main();
